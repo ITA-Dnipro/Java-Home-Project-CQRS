bd59f3c7416c6f5d3199cfda68b8b4bba2e7a27c
vzhyh <zhyhyr@progress.com>
2022-05-26 18:39:19 +0300

    replace imports

--- home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/kafka/configuration/KafkaProducerConfig.java
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/kafka/configuration/KafkaProducerConfig.java
@@ -1,5 +1,8 @@
 package com.softserveinc.ita.homeproject.homeservice.kafka.configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +11,6 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
