031e1016206f68ea0acfb6e651c101f2a972a35c
Kostiantyn Cheban <kcheb@softserveinc.com>
2022-05-27 01:26:18 +0300

    Add kafka reader/writer with dummy strings

---
+++ home-service/src/main/java/com/softserveinc/ita/homeproject/homeservice/kafka/AppEventProducer.java
@@ -0,0 +1,31 @@
+package com.softserveinc.ita.homeproject.homeservice.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(value = "home.kafka.enabled", havingValue = "true", matchIfMissing = true)
+public class AppEventProducer {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public AppEventProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendTestMessagesOnStartup() {
+        for (int i = 1; i <= 100; i++) {
+            sendEvent("Test message #" + i);
+        }
+    }
+
+    public void sendEvent(String message) {
+        kafkaTemplate.send("home", message);
+    }
+}
