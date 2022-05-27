53b44fca5ec9e914e2cbd7cb8e8bc5b7e75063ba
vzhyh <zhyhyr@progress.com>
2022-05-27 13:16:34 +0300

    fix some issues in pom.xml

--- home-events/src/main/java/com/softserve/ita/homeproject/events/NewsAddEvent.java
+++ home-events/src/main/java/com/softserve/ita/homeproject/events/NewsAddEvent.java
@@ -1,9 +1,9 @@
 package com.softserve.ita.homeproject.events;
 
+import java.time.LocalDateTime;
+
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
