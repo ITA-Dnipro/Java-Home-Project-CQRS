2ce524e4997c152adafe0b1ed34babdcef11689d
vzhyh <zhyhyr@progress.com>
2022-05-27 11:55:03 +0300

    finished module

--- home-events/src/main/java/com/softserve/ita/homeproject/events/NewsAddEvent.java
+++ home-events/src/main/java/com/softserve/ita/homeproject/events/NewsAddEvent.java
@@ -1,4 +1,28 @@
 package com.softserve.ita.homeproject.events;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class NewsAddEvent extends AppEvent{
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime updateDate;
+
+    private String title;
+
+    private String text;
+
+    private String description;
+
+    private String photoUrl;
+
+    private String source;
+
+    private Boolean enabled;
+
 }
