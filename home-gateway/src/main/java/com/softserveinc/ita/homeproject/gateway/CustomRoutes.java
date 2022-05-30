0df409a1559fb6921c94182aed3b6270e9aaca0e
Kostiantyn Cheban <kcheb@softserveinc.com>
2022-05-31 02:10:34 +0300

    forward GET /api/0/news to reader's /news

--- home-gateway/src/main/java/com/softserveinc/ita/homeproject/gateway/CustomRoutes.java
+++ home-gateway/src/main/java/com/softserveinc/ita/homeproject/gateway/CustomRoutes.java
@@ -1,21 +1,32 @@
 package com.softserveinc.ita.homeproject.gateway;
 
-import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties.ZuulRoute;
 import org.springframework.stereotype.Component;
 
+@Data
 @Component
 @ConfigurationProperties("home.gateway.custom-routes")
 public class CustomRoutes {
 
-    private List<String> reader;
+    @Data
+    public static class CustomRoute {
+        private ZuulRoute route;
 
-    public List<String> getReader() {
-        return reader;
+        private String method;
     }
 
-    public void setReader(List<String> reader) {
-        this.reader = reader;
+    private Map<String, CustomRoute> routes;
+
+    @PostConstruct
+    public void init() {
+        for(Map.Entry<String, CustomRoute> entry : routes.entrySet()) {
+            CustomRoute route = entry.getValue();
+            route.getRoute().setId(entry.getKey());
+        }
     }
 }
